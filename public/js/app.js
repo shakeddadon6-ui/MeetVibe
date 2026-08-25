4d074c7523b2244b1b9c7d7c8910d32bdf304f07
Shaked <shakeddadon6@gmail.com>
2026-08-26 00:11:06 +0300

    Force cache refresh and use CartoDB map for English

--- public/js/app.js
+++ public/js/app.js
@@ -55,14 +55,14 @@ function updateMapLanguage(lang) {
     if (currentTileLayer) { map.removeLayer(currentTileLayer); }
     
     if (lang === 'en') {
-        // Esri World Street Map (אנגלית)
-        currentTileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+        // מפה של CartoDB שמכריחה אנגלית בינלאומית (Voyager)
+        currentTileLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; CartoDB'
         });
     } else {
-        // OpenStreetMap (עברית בישראל)
+        // המפה הרגילה בעברית
         currentTileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; OpenStreetMap'
         });
     }
     currentTileLayer.addTo(map);
